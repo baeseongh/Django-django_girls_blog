299f3db4f6c8d4a2e4f454d62fcfcedef0478d2b
Seonghyon Bae <tjdgusdkf@likelion.org>
2019-05-27 11:28:23 +0900

    pu, delete

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False) #넘겨진 데이터를 바로 Post에 저장하지 말 것
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -31,9 +31,24 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_new.html', {'form' : form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts' : posts })
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=post.pk) 
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    
+    return redirect('post_list')
